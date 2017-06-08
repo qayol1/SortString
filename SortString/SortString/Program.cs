8ee098f66e856a867ca73231092deaf760babc36
qayol1 <qayol@freemail.hu>
2017-06-08 08:48:18 +0200

    string sorting implemented

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            Array.Sort(words);
+            String sorted = string.Join(" ", words);
+            Console.WriteLine(sorted);
+            Console.ReadKey();
+        }
+    }
+}
